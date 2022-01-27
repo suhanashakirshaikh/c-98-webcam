46b8cbbafdaf4f5b0f3a1d4e2cfa3d9e6690a041
suhanashakirshaikh <83486875+suhanashakirshaikh@users.noreply.github.com>
2022-01-27 13:03:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,20 +14,32 @@ recognition.onresult = function run (event){
 
     var Content = event.results[0][0].transcript;
     console.log(Content);
+    if(Content =="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
 
     document.getElementById("textbox").innerHTML = Content;
-    speak();
 
 }
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    
+    },5000);
+    
 }
 Webcam.Set({
     width:360,
@@ -36,3 +48,18 @@ Webcam.Set({
     png_quality:90
 });
 camera = document.getElementById("camera")
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+
+    });
+}
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
